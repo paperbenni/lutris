9c2e855102014e998e7786b54c980dade43e639f
paperbenni <paperbenni@gmail.com>
2019-09-10 22:20:53 +0200

    ssbb

---
+++ ssbb/infinite.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/paperbenni/bash/master/import.sh)
+pb lutris/dolphin
+fixgecko
+
+cd
+SCRIPTDIR=$(echo $0 | egrep -o '.*/')
+wall "$SCRIPTDIR"
+cd $SCRIPTDIR
+
+wiinsertsd "infinite.raw"
+wiinsertiso "ssbb.iso"
